89068cfe65284227bff1f13fedeb278151f28684
Warren R. Bank <warren.r.bank@gmail.com>
2019-04-30 15:21:25 -0700

    based on observed HTTP requests, make additional changes to headers

--- CastReceiver/js/receiver.js
+++ CastReceiver/js/receiver.js
@@ -10,12 +10,28 @@ const updateRefererHeader = loadRequestData => {
   if (loadRequestData.customData && loadRequestData.customData.referer) {
     refererHeader = loadRequestData.customData.referer
   }
+  else if (loadRequestData.media && loadRequestData.media.customData && loadRequestData.media.customData.referer) {
+    refererHeader = loadRequestData.media.customData.referer
+  }
   return loadRequestData
 }
 
+const headers_remove = ["chrome-proxy","cast-device-capabilities"]
+const headers_update = {
+  "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+}
+
 const updateNetworkRequestInfo = networkRequestInfo => {
-  if (refererHeader && networkRequestInfo && networkRequestInfo.headers) {
-    networkRequestInfo.headers.referer = refererHeader
+  if (networkRequestInfo && networkRequestInfo.headers) {
+    headers_remove.forEach(name => {
+      delete networkRequestInfo.headers[name]
+    })
+
+    Object.assign(networkRequestInfo.headers, headers_update)
+
+    if (refererHeader) {
+      networkRequestInfo.headers['referer'] = refererHeader
+    }
   }
   return networkRequestInfo
 }
