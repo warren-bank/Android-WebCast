e239a1c82a66246b1f3fe9f6d7b3b2f298c0bf4b
Warren R. Bank <warren.r.bank@gmail.com>
2020-08-23 07:14:11 -0700

    update the 'Exit' menu item to support API 21+

--- android-studio-project/WebCast/src/main/java/com/github/warren_bank/webcast/ExitActivity.java
+++ android-studio-project/WebCast/src/main/java/com/github/warren_bank/webcast/ExitActivity.java
@@ -2,6 +2,7 @@ package com.github.warren_bank.webcast;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,14 +10,25 @@ public class ExitActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        finish();
+
+        if (Build.VERSION.SDK_INT >= 21)
+            finishAndRemoveTask();
+        else
+            finish();
+
+        System.exit(0);
     }
 
     public static void exit(Context context) {
         Intent intent = new Intent(context, ExitActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+
+        if (Build.VERSION.SDK_INT >= 5)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+
         context.startActivity(intent);
     }
 }
